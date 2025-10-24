d93e2839665f83111a34fbbb911a77779fedb8f5
RomuloAmaral <romuloamaral32@gmail.com>
2025-10-24 00:31:02 -0300

    Parte da SideBar (ainda nao completa)

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
+import Sidebar from './Sidebar'
 
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}> 
     <Box sx={{height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
-      SideBar
+      <Sidebar/>
       <Typography variant='body2' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
         Feed
       </Typography>
     </Box>
-    //min 40 
+    
     </Stack>
   )
 }
