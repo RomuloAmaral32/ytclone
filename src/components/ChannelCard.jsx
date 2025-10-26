cf568054e83b5ff270219d37a61114dbd8014fbe
RomuloAmaral <romuloamaral32@gmail.com>
2025-10-25 23:32:23 -0300

    Finaliza o card de canal no Feed

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -8,10 +8,18 @@ const ChannelCard = ({channelDetail}) => {
   return(
     <Box
       sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: { xs: '356px', md: '320px' },
+        height: '326px',
+        margin: 'auto',
         boxShadow: 'none',
         borderRadius: '20px',
       }} >
-      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}
+        style={{ 
+          textDecoration: 'none' }}>
           <CardContent
             sx={{
               display: 'flex',
@@ -24,6 +32,15 @@ const ChannelCard = ({channelDetail}) => {
               image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
                 alt={channelDetail?.snippet?.title}
                 sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}/>
+            <Typography variant="h6" >
+              {channelDetail?.snippet?.title}
+              <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }}/>
+            </Typography>
+            {channelDetail?.statistics?.subscriberCount && (
+              <Typography sx={{ fontSize: '15px', fontWeight: 500, color: 'gray' }}>
+                {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString('en-US')} Subscribers
+              </Typography>
+            )}
 
               </CardContent>
       </Link>
